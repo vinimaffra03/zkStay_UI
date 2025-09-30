bcf507ce0501a91abea92fd38b1d56a3341c54e5
vinimaffra03 <viniciushmdem15@gmail.com>
2025-09-30 16:02:21 -0300

    logo title

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,8 +1,12 @@
+import house from "../Login/house.jpg";
+
 export const Logo = () => {
   return (
-    <div className="flex items-center gap-1">
-      <span className="text-2xl font-semibold text-grey-500">
-        <strong>zk</strong>Stay
+    <div className="flex items-center">
+      <img src={house} alt="logo" className="w-16 h-auto" />
+      <span className="text-2xl font-semibold text-dark-900">
+        <strong>zk</strong>
+        <span className="text-gray-600">Stay</span>
       </span>
     </div>
   );
